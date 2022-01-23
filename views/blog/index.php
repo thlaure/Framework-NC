0b2276ffb698b7e0befed4b19771c8146d7c646f
Thomas LAURE <thomas.laure@ik.me>
2022-01-23 17:24:32 +0100

    Video 3

---
+++ views/blog/index.php
@@ -0,0 +1,2 @@
+<h1>Homepage</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam sit in repellat suscipit? Commodi sunt quos, illum nisi exercitationem inventore illo perspiciatis molestiae debitis aspernatur, labore repellat recusandae? Fuga, dolor!</p>
---
+++ views/layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon site</title>
+    <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'style.css' ?>">
+</head>
+<body>
+    <div class="container">
+        <?= $content ?>
+    </div>
+</body>
+</html>
