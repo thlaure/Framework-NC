1f78b2b4467b05a128178b45cbd312ffa147f9d5
Thomas LAURE <thomas.laure@ik.me>
2022-01-26 21:09:39 +0100

    Video 7

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use PDO;
 use Database\DBConnection;
 
 abstract class Model
@@ -17,12 +18,14 @@ abstract class Model
     public function all(): array
     {
         $stmt = $this->db->getPDO()->query("SELECT * FROM {$this->table} ORDER BY created_at DESC");
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_class($this), [$this->db]);
         return $stmt->fetchAll();
     }
 
-    public function findById(int $id)
+    public function findById(int $id): self
     {
         $stmt = $this->db->getPDO()->prepare("SELECT * FROM {$this->table} WHERE id = ?");
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_class($this), [$this->db]);
         $stmt->execute([$id]);
         return $stmt->fetch();
     }
